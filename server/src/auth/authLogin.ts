a464e60d2ff14395a7fa1f461ff33cd9575d0fcb
dzl-i <denzel.iskandar@gmail.com>
2023-10-13 03:15:05 +1100

    Correct error message

--- server/src/auth/authLogin.ts
+++ server/src/auth/authLogin.ts
@@ -5,12 +5,12 @@ import { getUserByEmail } from "../helper/userHelper";
 export async function authLogin(email: string, password: string) {
   // Error Handling
   if (await checkBlockedAccount(email)) throw { status: 400, message: "You have entered an incorrect password for three times, so the account has been blocked. Please reset your password." };
-  if (! await checkEmailExists(email)) throw { status: 400, message: "Email does not exist" };
-  if (! await verifyLogin(email, password)) throw { status: 400, message: "Password is not correct" };
+  if (! await checkEmailExists(email)) throw { status: 400, message: "Email does not exist." };
+  if (! await verifyLogin(email, password)) throw { status: 400, message: "Password is not correct." };
 
   // Get User
   const user = await getUserByEmail(email);
-  if (user === null) throw { status: 400, message: "Email does not exist" };
+  if (user === null) throw { status: 400, message: "Email does not exist." };
   const { accessToken, refreshToken } = await generateToken(user.id);
 
   return {
